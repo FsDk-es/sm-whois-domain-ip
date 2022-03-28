0eddc6bf2fc167a4a238758ab386716903f07dee
FsDk <cayuela@gmail.com>
2022-03-28 02:27:27 +0200

    se mejora la estructura del codigo bastante y se agregan nuevas caracteristicas.

--- src/sistema/index.js
+++ src/sistema/index.js
@@ -2,27 +2,34 @@
 FsDk - Jose Antonio Gimenez Cayuela 
 Simple app para obtener informacion de 1 dominio
 */
-let config1 = require('../../config.js');
-console.log(config);
 
+//importamos 1 variable. del archivo de configuracion
+import {config} from '../../config.js';
+// 
+// importamos la libreria node-fetch
 import fetch from 'node-fetch';
+//
 let dominioAconsultar = process.argv[2];
-console.log(dominioAconsultar);
+//console.log(dominioAconsultar);
 let peticion = dominioAconsultar
 const urlConsulta = 'https://api.ip2whois.com/v2?';
-const keyToken = 'HZRMIA2DI3YMZGIU2WZLPUC8YXHBYYPF';
+const keyToken = config.key
 const intermedioConsulta = '&'
 const palabraKey = 'key=';
 const palabraDomain = 'domain='
 
 
-/*
+
 //debug/
 const response = await fetch(urlConsulta + palabraKey + keyToken + intermedioConsulta + palabraDomain + peticion);
 const consulta = await response.json();
-console.log(urlConsulta + palabraKey + keyToken + intermedioConsulta + palabraDomain + peticion);
-*/
-/*
+//console.log(urlConsulta + palabraKey + keyToken + intermedioConsulta + palabraDomain + peticion);
+
+if (consulta.error != null) {
+    console.log('Codigo de error' + ' ' + consulta.error.error_code);
+    console.log('Mensaje de error:' + ' ' + consulta.error.error_message);
+    process.exit(); 
+}
 
 let nombreDeDominio = consulta['domain'];
 let idDominio = consulta['domain_id'];
@@ -31,18 +38,19 @@ let creacionDominio = consulta['create_date'];
 let ultimaActualizacion = consulta['update_date'];
 let expiracion = consulta['expire_date'];
 let provincia = consulta['registrant'].region;
+ 
 let pais = consulta['registrant'].country;
 let servidoresNS = consulta['nameservers'];
-*/
-/*console.log('Nombre de Dominio:' + ' ' + nombreDeDominio);
+
+console.log('Nombre de Dominio:' + ' ' + nombreDeDominio);
 console.log('ID Del dominio:' + ' ' + idDominio);
 console.log('Estado del dominio:' + ' ' + estadoDominio);
 console.log('Creacion del dominio:' + ' ' + creacionDominio);
 console.log('Ultima actualizacion:' + ' ' + ultimaActualizacion);
 console.log('Fecha de expiracion:' + ' ' + expiracion);
 console.log('Provincia del registrante:' + ' ' + provincia);
-console.log('Pais del registrante:' + pais);
+console.log('Pais del registrante:' + ' ' + pais);
 console.log('Servidores DNS:' + ' ' + servidoresNS);
 console.log('========Fin de la consulta==========');
-*/
+
 //console.log(consulta);
